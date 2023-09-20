f428c01b131c8fed9164a40ef6ef12c3a6f3af25
SaifAbuR3d <saefraed2@gmail.com>
2023-09-20 07:20:11 +0300

    fix query - Preferred Genre Of Different Age Groups

--- Scripts/Preferred_Genre_Of_Different_Age_Groups_Query.sql
+++ Scripts/Preferred_Genre_Of_Different_Age_Groups_Query.sql
@@ -1,4 +1,3 @@
-
 with Borrower_AgeGroup(BorrowerID, AgeGroup) as (
     select
 		b.BorrowerID,
@@ -11,25 +10,26 @@ with Borrower_AgeGroup(BorrowerID, AgeGroup) as (
 				WHEN DATEDIFF(YEAR, B.[Date Of Birth], GETDATE()) BETWEEN 51 AND 60 THEN '51-60'
 				WHEN DATEDIFF(YEAR, B.[Date Of Birth], GETDATE()) BETWEEN 61 AND 70 THEN '61-70'
 				WHEN DATEDIFF(YEAR, B.[Date Of Birth], GETDATE()) BETWEEN 71 AND 80 THEN '71-80'
-				WHEN DATEDIFF(YEAR, B.[Date Of Birth], GETDATE()) BETWEEN 31 AND 40 THEN '81-90'
+				WHEN DATEDIFF(YEAR, B.[Date Of Birth], GETDATE()) BETWEEN 81 AND 90 THEN '81-90'
 				ELSE 'Too Old'
 			END 
 		as AgeGroup
 	from Borrowers as b
 	), 
 
-Genre_AgeGroup_Score(AgeGroup, Genre, Score) as (
-	select ba.AgeGroup, b.Genre, COUNT(*) as Score
+Genre_AgeGroup_NumberOfLoans(AgeGroup, Genre, [Number of Loans]) as (
+	select ba.AgeGroup, b.Genre, COUNT(*) as [Number of Loans]
 	from Borrower_AgeGroup as ba join Loans as l on ba.BorrowerID = l.BorrowerID 
 	join Books as b on l.BookID = b.BookID  
 	group by ba.AgeGroup, b.Genre
-	), 
+	),
 
 Genre_AgeGroup_Rank (AgeGroup, Genre, [Genre Rank]) as (
-    select AgeGroup, Genre, ROW_NUMBER() OVER (PARTITION BY AgeGroup ORDER BY Score DESC) AS GenreRank
-    from  Genre_AgeGroup_Score
+    select AgeGroup, Genre, ROW_NUMBER() OVER (PARTITION BY AgeGroup ORDER BY [Number Of Loans] DESC) AS GenreRank
+    from  Genre_AgeGroup_NumberOfLoans
 )
 
 select AgeGroup, Genre
 from Genre_AgeGroup_Rank 
 where [Genre Rank] = 1 
+;
