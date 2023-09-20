5c5c22cec9f387e67ba1c9e9526468fea1828c10
SaifAbuR3d <saefraed2@gmail.com>
2023-09-20 06:51:06 +0300

    fix query - Borrowing_Frequency_Rank

--- Scripts/Borrowing_Frequency_Rank_Query.sql
+++ Scripts/Borrowing_Frequency_Rank_Query.sql
@@ -1,6 +1,6 @@
 select B.[BorrowerID], B.[First Name], B.[Last Name], 
 COUNT(L.LoanID) as BorrowingFrequency,
-RANK() over (order by COUNT(*) desc) as BorrowingRank
+RANK() over (order by COUNT(L.LoanID) desc) as BorrowingRank
 from Borrowers as B LEFT JOIN Loans as L
 on B.BorrowerID = L.BorrowerID
 group by B.BorrowerID, B.[First Name], B.[Last Name]
